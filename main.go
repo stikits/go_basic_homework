110f3791374a5e1c74280f32f883ee8c65568336
stikits <stikbomb@gmail.com>
2025-02-23 15:42:10 +0300

    add input and calculate templates

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
+import "fmt"
+
+func getInput() string {
+    var result string
+    fmt.Println("Введите что-нибудь:")
+    fmt.Scan(&result)
+    return result
+}
+
 func main()  {
     fromUstToEur := 0.96
     fromUsdToRub := 88.45
     fromEurToRub := (1 / fromUstToEur) * fromUsdToRub
+    fmt.Println(fromEurToRub)
+    inputText := getInput()
+    fmt.Println(inputText)
 }
+
+func calculate(amount float64, sourceCurrency string, targetCurrency string) float64 {
+    return 0
+} 
